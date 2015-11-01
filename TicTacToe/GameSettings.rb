30437555fe47aed517449ad14159421d175af5b2
Simon Tsai <simon.tsai.j@gmail.com>
2015-11-01 16:40:35 -0500

    GameSettings holds the game type, first player, and the marker for the first player.  It will always be used to instantiate and return an instance of Game later.

---
+++ TicTacToe/GameSettings.rb
@@ -0,0 +1,7 @@
+class GameSettings
+  def initialize(game_type, first_player, first_player_marker)
+    @game_type = game_type
+    @first_player = first_player
+    @first_player_marker = first_player_marker
+  end
+end
