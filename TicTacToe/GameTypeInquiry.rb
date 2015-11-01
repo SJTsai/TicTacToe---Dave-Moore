7294bedffbe2734aca94fed18a4ca3c90fd82fa7
Simon Tsai <simon.tsai.j@gmail.com>
2015-11-01 01:47:06 -0400

    Added a class that takes care of the game-type-related inquiry.  It prompts the user to choose one of three game types and handles wrong input.  It returns one of the three symbols:
    :HumanVersusHuman
    :HumanVersusComputer
    :ComputerVersusComputer

---
+++ TicTacToe/GameTypeInquiry.rb
@@ -0,0 +1,59 @@
+class GameTypeInquiry
+  public
+  def inquire_game_type
+    display_straightforward_inquiry
+    user_option = get_user_option
+    
+    until is_valid_option?(user_option)
+      display_wrong_input_inquiry
+      user_option = get_user_option
+    end
+    
+    return game_type_for_option(user_option)
+  end
+  
+  private
+  def display_straightforward_inquiry
+    puts "Please choose a game type:"
+    display_options
+    puts 
+  end
+  
+  private
+  def display_options
+    puts "(1) Human vs. Human"
+    puts "(2) Human vs. Computer"
+    puts "(3) Computer vs. Computer"
+  end
+  
+  private
+  def get_user_option
+    user_option = gets.chomp
+    puts
+    return user_option  
+  end
+  
+  private
+  def is_valid_option?(option)
+    return option == "1" || option == "2" || option == "3"
+  end
+  
+  private
+  def display_wrong_input_inquiry
+    puts "You must choose 1, 2, or 3."
+    display_options
+    puts
+  end
+  
+  private
+  def game_type_for_option(option)
+    if option == "1"
+      return :HumanVersusHuman
+    elsif option == "2"
+      return :HumanVersusComputer
+    elsif option == "3"
+      return :ComputerVersusComputer
+    end
+  end
+end
+
