f463a548cae8c9b096424f56dd6b7deffc420c35
Simon Tsai <simon.tsai.j@gmail.com>
2015-11-01 20:14:31 -0500

    Added a function that returns the player type (human or computer) for the first and second player.

--- TicTacToe/game_settings.rb
+++ TicTacToe/game_settings.rb
@@ -6,4 +6,22 @@ class GameSettings
     @first_player = first_player
     @first_player_marker = first_player_marker
   end
+  
+  public
+  def player_type_for_first_player
+    if game_type == :HumanVersusHuman || game_type == :HumanVersusComputer
+      return :Human
+    elsif game_type == :ComputerVersusComputer
+      return :Computer
+    end
+  end
+  
+  public
+  def player_type_for_second_player
+    if game_type == :HumanVersusHuman
+      return :Human
+    elsif game_type == :HumanVersusComputer || game_type == :ComputerVersusComputer
+      return :Computer
+    end
+  end
 end
