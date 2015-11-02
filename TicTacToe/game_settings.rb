f24763ce0c24cf14bc19c6fb05157b2fb9777e99
Simon Tsai <simon.tsai.j@gmail.com>
2015-11-01 20:25:36 -0500

    Modified name to make it clearer.

--- TicTacToe/game_settings.rb
+++ TicTacToe/game_settings.rb
@@ -1,9 +1,9 @@
 class GameSettings
-  attr_reader :game_type, :first_player, :first_player_marker
+  attr_reader :game_type, :first_player_type, :first_player_marker
   
-  def initialize(game_type, first_player, first_player_marker)
+  def initialize(game_type, first_player_type, first_player_marker)
     @game_type = game_type
-    @first_player = first_player
+    @first_player_type = first_player_type
     @first_player_marker = first_player_marker
   end
   
